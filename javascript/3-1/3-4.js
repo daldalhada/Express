a97446703d0236a270d06923df44e3c21d7299bd
daldalhada <ssen199545@gmail.com>
2021-01-07 01:06:33 +0900

    add process javascript code

---
+++ javascript/3-1/3-4.js
@@ -0,0 +1,7 @@
+console.log(process.version);   // v14.15.4
+console.log(process.arch);      // x64
+console.log(process.platform);  // win32
+console.log(process.pid);       // 20348
+console.log(process.uptime());  // 0.9734597
+console.log(process.execPath);  //C:\Program Files\...
+console.log(process.cpuUsage());  // {user:2100000, system: 2030000}
