9428b5be61da7faa5def0bc0ff401ca9d2a42876
daldalhada <ssen199545@gmail.com>
2021-01-09 23:40:23 +0900

    add package && update app.js

--- learn-express/app.js
+++ learn-express/app.js
@@ -1,18 +1,33 @@
 const express = require('express');
 const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 
 const app = express();
 
 // 서버에 port라는 속성을 삽입(전역변수의 역할)
 app.set('port', process.env.PORT || 3000);
 
-app.use((req, res, next) => {
-    console.log('모든 요청에 실행');
-    next();
-})
+app.use(morgan('dev'));     // dev 대신 combined를 쓰면 더 자세해짐
+app.use(cookieParser('password'));
+app.use(express.json());    // bodyParser를 대체
+app.use(express.urlencoded({ extended: true }));    // true면 qs, false면 querystring
 
 app.get('/', (req, res) => {
-    //res.sendFile('./index.html)
+    req.body.name;
+
+    req.cookies;
+    req.signedCookies;      // 암호화된 쿠키
+
+    res.cookie('name', encodeURIComponent(name), {
+        expires: new Date(),
+        httpOnly: true,
+        path: '/',
+    })
+    res.clearCookie('name', encodeURIComponent(name), {
+        httpOnly: true,
+        path: '/',
+    })
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
