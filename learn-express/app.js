acf29743aa7367eef0f075c2d272617af5eec5c2
daldalhada <ssen199545@gmail.com>
2021-01-09 22:18:47 +0900

    update express code

--- learn-express/app.js
+++ learn-express/app.js
@@ -6,6 +6,11 @@ const app = express();
 // 서버에 port라는 속성을 삽입(전역변수의 역할)
 app.set('port', process.env.PORT || 3000);
 
+app.use((req, res, next) => {
+    console.log('모든 요청에 실행');
+    next();
+})
+
 app.get('/', (req, res) => {
     //res.sendFile('./index.html)
     res.sendFile(path.join(__dirname, 'index.html'));
@@ -15,9 +20,21 @@ app.post('/', (req, res) => {
     res.send('hello express');
 });
 
-app.get('/about', (req, res) => {
-    res.send('hello express');
+app.get('/category/javascript', (req, res) => {
+    res.send(`hello javascript`);
 });
+
+app.get('/category/java', (req, res) => {
+    res.send(`hello java`);
+});
+
+app.get('/category/:name', (req, res) => {
+    res.send(`hello ${req.params.name}`);
+});
+
+app.get('*', (req, res) => {
+    res.send('hello everyday');
+})
 
 app.listen(app.get('port'), () => {
     console.log('express server on');
