a23f04b7336a39ef45a3f5aeadde6f71c5e17de5
daldalhada <ssen199545@gmail.com>
2021-01-14 13:16:51 +0900

    modify page.js(post)

--- project/NodeBird/routes/page.js
+++ project/NodeBird/routes/page.js
@@ -1,30 +1,43 @@
 const express = require('express');
+const { isLoggedIn, isNotLoggedIn } = require('./middlewares');
+const { Post, User } = require('../models');
 
 const router = express.Router();
 
+// 같은 변수를 대부분의 라우터의 넣는 경우 변수를 따로 미들웨어로 뺌(중복 제거)
 router.use((req, res, next) => {
-  res.locals.user = null;
+  res.locals.user = req.user;
   res.locals.followerCount = 0;
   res.locals.followingCount = 0;
   res.locals.followerIdList = [];
   next();
 });
 
-router.get('/profile', (req, res) => {
+router.get('/profile', isLoggedIn, (req, res) => {
   res.render('profile', { title: '내 정보 - NodeBird' });
 });
 
-router.get('/join', (req, res) => {
+router.get('/join', isNotLoggedIn, (req, res) => {
   res.render('join', { title: '회원가입 - NodeBird' });
 });
 
-router.get('/', (req, res, next) => {
-  const twits = [];                 // 메인 게시물
-  res.render('main', {
-    title: 'NodeBird',
-    twits,
-    user: req.user,
-  });
+router.get('/', async (req, res, next) => {
+  try {
+    const posts = await Post.findAll({
+      include: {
+        model: User,
+        attributes: ['id', 'nick'],
+      },
+      order: [['createdAt', 'DESC']],
+    });
+    res.render('main', {
+      title: 'NodeBird',
+      twits: posts,
+    });
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
 });
 
 module.exports = router;
