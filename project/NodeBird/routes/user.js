fb8e6ae90a4618c13a45c9ed92056cb6aa747060
daldalhada <ssen199545@gmail.com>
2021-01-14 14:53:13 +0900

    add comment

--- project/NodeBird/routes/user.js
+++ project/NodeBird/routes/user.js
@@ -5,11 +5,12 @@ const User = require('../models/user');
 
 const router = express.Router();
 
+// POST /user/1/follow
 router.post('/:id/follow', isLoggedIn, async (req, res, next) => {
   try {
     const user = await User.findOne({ where: { id: req.user.id } });
     if (user) {
-      await user.addFollowing(parseInt(req.params.id, 10));
+      await user.addFollowings([parseInt(req.params.id, 10)]);
       res.send('success');
     } else {
       res.status(404).send('no user');
