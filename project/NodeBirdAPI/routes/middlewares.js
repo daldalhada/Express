a08cace9ee004deabfdfb3293b8e9adc16c08c39
daldalhada <ssen199545@gmail.com>
2021-01-15 16:22:38 +0900

    require jwt

--- project/NodeBirdAPI/routes/middlewares.js
+++ project/NodeBirdAPI/routes/middlewares.js
@@ -1,18 +1,19 @@
+const jwt = require('jsonwebtoken');
+
 exports.isLoggedIn = (req, res, next) => {
-    if (req.isAuthenticated()) {
-      next();
-    } else {
-      res.status(403).send('로그인 필요');
-    }
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    res.status(403).send('로그인 필요');
+  }
 };
-  
+
 exports.isNotLoggedIn = (req, res, next) => {
-    if (!req.isAuthenticated()) {
-      next();
-    } else {
-      const message = encodeURIComponent('로그인한 상태입니다.');
-      res.redirect(`/?error=${message}`);
-    }
+  if (!req.isAuthenticated()) {
+    next();
+  } else {
+    res.redirect('/');
+  }
 };
 
 exports.verifyToken = (req, res, next) => {
